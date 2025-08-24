d894218f53f8135aad8a37bdd3ada0335cfabbe5
Cliff Pan <cliffordpan.helenchen@gmail.com>
2025-08-24 12:09:19 -0700

    bugfix

--- lib/src/main/kotlin/me/hchome/kactor/impl/BaseActor.kt
+++ lib/src/main/kotlin/me/hchome/kactor/impl/BaseActor.kt
@@ -223,8 +223,12 @@ internal class BaseActor(
         actorSystem.recover(ref, attributes)
     }
 
-    override suspend fun snapshot(child: ActorRef): Attributes {
-        return actorSystem.snapshot(ref)
+    override suspend fun snapshot(child: ActorRef): Attributes? {
+        return try {
+            actorSystem.snapshot(ref)
+        } catch (_: Throwable) {
+            null
+        }
     }
 
     override suspend fun supervise(
@@ -238,7 +242,9 @@ internal class BaseActor(
                 val attributes = snapshot(child)
                 actorSystem.destroyActor(child)
                 val ref = actorSystem.actorOfSuspend(child.rawId, ref, child.handler)
-                recover(ref, attributes)
+                if (attributes != null) {
+                    recover(ref, attributes)
+                }
             }
 
             is AllForOne -> {
@@ -250,7 +256,9 @@ internal class BaseActor(
                 children.forEach {
                     val attr = snapshot(it)
                     val childRef = actorSystem.actorOfSuspend(it.rawId, ref, it.handler)
-                    recover(childRef, attr)
+                    if (attr != null) {
+                        recover(childRef, attr)
+                    }
                 }
             }
 
